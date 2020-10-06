6419210c68ae2fe77150facdcaedaa8131bd666e
bhargavmln <bhargavmln@gmail.com>
2020-10-07 02:14:38 +0530

    [bhargavmln]Refactor: Refactored by adding constructor with parameter and handled null pointer exception

--- src/main/java/com/Capgemini/com/moodAnalyser/MoodAnalyser.java
+++ src/main/java/com/Capgemini/com/moodAnalyser/MoodAnalyser.java
@@ -5,24 +5,36 @@ import java.util.Scanner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 public class MoodAnalyser {
-	
+
+	String message;
+
+	public MoodAnalyser() {
+	}
+
+	public MoodAnalyser(String message) {
+		this.message = message;
+	}
+
 	private static Logger LOG = LogManager.getLogger(MoodAnalyser.class);
-	
-	public String analyseMood(String message) {
-		if (message.contains("Sad")|| message.contains("sad")) {
-			return "SAD";
-		} else {
+
+	public String analyseMood() {
+		try {
+			if (message.contains("Sad") || message.contains("sad")) {
+				return "SAD";
+			} else {
+				return "HAPPY";
+			}
+		} catch (NullPointerException e) {
 			return "HAPPY";
 		}
 	}
 
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		MoodAnalyser moodAnalyser = new MoodAnalyser();
 		LOG.info("Enter your Mood: ");
 		String message = sc.nextLine();
-		LOG.info(moodAnalyser.analyseMood(message));
+		MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+		LOG.info(moodAnalyser.analyseMood());
 	}
 }
--- src/test/java/com/Capgemini/com/moodAnalyser/MoodAnalyserTest.java
+++ src/test/java/com/Capgemini/com/moodAnalyser/MoodAnalyserTest.java
@@ -1,29 +1,30 @@
 package com.Capgemini.com.moodAnalyser;
 
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class MoodAnalyserTest 
-{
-	static MoodAnalyser myMood = null;
-	
-	@BeforeClass
-	public static void setUp() {
-	myMood = new MoodAnalyser();
+public class MoodAnalyserTest {
+
+	private String Null;
+
+	@Test
+	public void givenMessage_whenSad_returnSad() {
+		MoodAnalyser myMood = new MoodAnalyser("I am in Sad Mood");
+		String mood = myMood.analyseMood();
+		Assert.assertEquals("SAD", mood);
+	}
+
+	@Test
+	public void givenMessage_whenNotSad_returnSad() {
+		MoodAnalyser myMood = new MoodAnalyser("I am in Happy Mood");
+		String mood = myMood.analyseMood();
+		Assert.assertEquals("HAPPY", mood);
 	}
 	
-    @Test
-    public void givenMessage_whenSad_returnSad()
-    {
-       String mood = myMood.analyseMood("I am in Sad Mood");
-       Assert.assertEquals("SAD", mood);
-    }
-    
-    @Test
-    public void givenMessage_whenNotSad_returnSad()
-    {
-       String mood = myMood.analyseMood("I am in Any Mood");
-       Assert.assertEquals("HAPPY", mood);
-    }
+	@Test
+	public void givenMessage_whenNull_returnHappy() {
+		MoodAnalyser myMood = new MoodAnalyser(null);
+		String mood = myMood.analyseMood();
+		Assert.assertEquals("HAPPY", mood);
+	}
 }
